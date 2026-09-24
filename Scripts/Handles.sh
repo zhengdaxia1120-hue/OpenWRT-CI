4db81df84ffb13cf3d3900e483741e216084b46c
zhengdaxia1120-hue <zhengdaxia1120@gmail.com>
2026-09-24 20:03:05 +0800

    Update Handles.sh

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -5,7 +5,7 @@
 FEEDS_PATH="./feeds"
 PACKAGE_PATH="./package"
 
-#修改argon主题字体和颜色
+# 修改 Argon 主题字体和颜色
 if [ -d "$PACKAGE_PATH/luci-theme-argon" ]; then
 	echo " "
 	if sed -i "s/primary '.*'/primary '#31a1a1'/g; s/'0.2'/'0.5'/g; s/'none'/'bing'/g; s/'600'/'normal'/g" \
@@ -16,40 +16,7 @@ if [ -d "$PACKAGE_PATH/luci-theme-argon" ]; then
 	fi
 fi
 
-#修改aurora菜单式样
-if [ -d "$PACKAGE_PATH/luci-app-aurora-config" ]; then
-	echo " "
-	if find "$PACKAGE_PATH/luci-app-aurora-config/root/usr/share/aurora/" -type f -name '*.template' -exec \
-		sed -i "s/nav_type '.*'/nav_type 'dropdown'/g; s/struct_radius_base '.*'/struct_radius_base '0.125rem'/g" {} +; then
-		echo "theme-aurora has been fixed!"
-	else
-		echo "theme-aurora fix failed; continuing!"
-	fi
-fi
-
-#修改mini-diskmanager菜单位置
-if [ -d "$PACKAGE_PATH/luci-app-mini-diskmanager" ]; then
-	echo " "
-	if sed -i "s/services/system/g" \
-		"$PACKAGE_PATH/luci-app-mini-diskmanager/luci-app-mini-diskmanager/root/usr/share/luci/menu.d/luci-app-mini-diskmanager.json"; then
-		echo "mini-diskmanager has been fixed!"
-	else
-		echo "mini-diskmanager fix failed; continuing!"
-	fi
-fi
-
-#修改natmapt菜单位置
-if [ -d "$PACKAGE_PATH/luci-app-natmapt" ]; then
-	echo " "
-	if sed -i "s/network/services/g" \
-		"$PACKAGE_PATH/luci-app-natmapt/root/usr/share/luci/menu.d/luci-app-natmap.json"; then
-		echo "natmapt has been fixed!"
-	else
-		echo "natmapt fix failed; continuing!"
-	fi
-fi
-
-#修复Rust编译失败
+# 修复 Rust 编译失败
 if [ -d "$FEEDS_PATH/packages/lang/rust" ]; then
 	echo " "
 	if sed -i 's/ci-llvm=true/ci-llvm=false/g' \
